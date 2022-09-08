3db18c143052c12f4520872fb34b69f7b7997422
Mahmoud Abukhatro <mahmoudabukhatro@GB-MABUKHATRO.local>
2022-09-08 15:04:18 +0200

    content type became JSON

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 
 const http = require('http');
-const { url } = require('inspector');
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
+    //res.setHeader('Content-Type', 'application/json');
+    //res.writeHead(200);
+
   console.log(req.url);
   if (req.url=="/api/hello") {
+
     var words = ['Rock', 'Paper', 'Scissors'];
     var number = Math.floor(Math.random() * words.length);
-    var word = words[number];
-
-    res.end( ' The Random word is :  ' + word); }
+    var output = {
+      "word":words[number]
+    };
+    res.statusCode = 200; 
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(output));
+  }
   else {
     res.end('Hello, World');}
 }
 
 const server = http.createServer(requestListener);
 server.listen(8080);
-
-
 
     //const randomn=Math.random()*20;
     //const randomn = Math.trunc(Math.random()*100)+1;
@@ -31,3 +35,7 @@ server.listen(8080);
     //var e = "Scissors";
     //var thing = window['cde'.charAt(Math.floor(Math.random()*3))];
     //document.write('The computer chose: ' + thing);
+
+    
+
+    
