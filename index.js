5a23dcf94315f4ead0570caebf71c8527a4f5d41
Mahmoud Abukhatro <mahmoudabukhatro@GB-MABUKHATRO.local>
2022-09-08 11:30:32 +0200

    i just added some random word working with some random number

--- index.js
+++ index.js
@@ -5,23 +5,29 @@ const { url } = require('inspector');
 const requestListener = function (req, res) {
   res.writeHead(200);
   console.log(req.url);
-if (req.url=="/api/hello")
+  if (req.url=="/api/hello") {
+    var words = ['Rock', 'Paper', 'Scissors'];
+    var number = Math.floor(Math.random() * words.length);
+    var word = words[number];
 
-
-{
-
-
-   //const randomn=Math.random()*20;
-   const randomn = Math.trunc(Math.random()*100)+1;
-
-res.end( ' The Random number between 1 and 100 is :            ' +randomn.toString());
-}
-else{
-    res.end('Hello, World');
-}
-  
+    res.end( ' The Random word is :  ' + word); }
+  else {
+    res.end('Hello, World');}
 }
 
 const server = http.createServer(requestListener);
-
 server.listen(8080);
+
+
+
+    //const randomn=Math.random()*20;
+    //const randomn = Math.trunc(Math.random()*100)+1;
+    //var things = ['Rock', 'Paper', 'Scissor'];
+    //var thing = things[Math.floor(Math.random()*things.length)];
+    //alert('The computer chose:' + thing);
+
+    //var c = "Rock";
+    //var d = "Paper";
+    //var e = "Scissors";
+    //var thing = window['cde'.charAt(Math.floor(Math.random()*3))];
+    //document.write('The computer chose: ' + thing);
